e8861db396f5d2e2c1743001af81ca4c8cee299f
DESKTOP-2VQTDIS\syogu <syogunn519@gmail.com>
2020-08-22 21:29:24 +0900

    タイトルUI用のイベント作成 ゲーム終了、ゲームシーンのロード、ロード後の処理追加が可能

---
+++ Assets/AntDiary/Scripts/UI/TitleUI.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleUI : MonoBehaviour
+{
+
+    /// <summary>
+    /// ゲームの終了
+    /// </summary>
+    public void GameExit()
+    {
+        Application.Quit();
+    }
+
+    /// <summary>
+    /// ゲームシーンの読み込み
+    /// </summary>
+    public void LoadGameScene()
+    {
+        //ロード後のイベント登録
+        SceneManager.sceneLoaded += GameSceneLoaded;
+
+        //ゲームシーンのロード(シーン名は仮置き)
+        SceneManager.LoadScene("GameScene");
+    }
+
+    /// <summary>
+    /// シーン読み込み後に何か処理させるための関数
+    /// </summary>
+    /// <param name="nextscene"></param>
+    /// <param name="mode"></param>
+    public void GameSceneLoaded(Scene nextscene,LoadSceneMode mode)
+    {
+        //何かしたければここに書く
+
+        //ロード後イベントからこの関数の削除
+        SceneManager.sceneLoaded -= GameSceneLoaded;
+    }
+}
